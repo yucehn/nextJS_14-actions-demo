d5aed5f581287563f993582ddaee2916e5c4fb49
Yu-Chen <yuyuchen@mail.7-11.com.tw>
2023-12-06 17:25:52 +0800

    feat: Server Actions & useAction abstraction & Form Components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,20 +1,40 @@
 "use client"
 
-import { create } from "@/actions/create-board";
-import { useFormState } from "react-dom";
-import { FormInput } from "./form-input";
-import { FormButton } from "./form-button";
+import { createBoard } from "@/actions/create-board";
+import { useAction } from "@/hooks/ues-action";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
-  const initialState = { message: "", errors: {}};
-  const [state, dispatch] = useFormState(create, initialState);
+  // const initialState = { message: "", errors: {}};
+  // const [state, dispatch] = useFormState(create, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess(data) {
+      console.log(data, 'Success');
+    },
+    onError(error) {
+      console.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData)=>{
+    const title = formData.get("title") as string;
+
+    execute({title});
+  }
 
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
         <div className="flex flex-col space-y-2">
-          <FormInput errors={state?.errors} />
+          <FormInput
+            id="title"
+            label="Board Title"
+            errors={fieldErrors}
+          />
         </div>
-        <FormButton />
+        <FormSubmit>
+          save
+        </FormSubmit>
       </form>
   )
 }
---
+++ components/form/form-submit.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useFormStatus } from "react-dom";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface FormSubmitProps{
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "primary"
+};
+
+export const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant
+}:FormSubmitProps)=>{
+  const { pending } = useFormStatus();
+
+  return (
+    <Button 
+      disabled={pending || disabled}
+      className={cn(className)}
+      variant={variant}
+      type="submit"
+      size="sm"
+    >
+      {children}
+    </Button>
+  )
+}
